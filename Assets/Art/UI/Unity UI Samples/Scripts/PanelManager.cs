80a31d2206e1990404cb784da37787cb4ec013aa
sungilan <asiverver@naver.com>
2024-09-11 11:38:59 +0900

    v1.0.3

---
+++ Assets/Art/UI/Unity UI Samples/Scripts/PanelManager.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+public class PanelManager : MonoBehaviour {
+
+// 이동할 씬의 이름을 Inspector에서 설정할 수 있도록 합니다.
+    public string targetSceneName;
+	public Animator initiallyOpen;
+
+	private int m_OpenParameterId;
+	private Animator m_Open;
+	private GameObject m_PreviouslySelected;
+
+	const string k_OpenTransitionName = "Open";
+	const string k_ClosedStateName = "Closed";
+
+	public void OnButtonClick()
+    {
+        // targetSceneName이 지정되었는지 확인
+        if (!string.IsNullOrEmpty(targetSceneName))
+        {
+            // 지정된 씬으로 이동
+            SceneManager.LoadScene(targetSceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Target scene name is not specified.");
+        }
+    }
+	public void OnEnable()
+	{
+		m_OpenParameterId = Animator.StringToHash (k_OpenTransitionName);
+
+		if (initiallyOpen == null)
+			return;
+
+		OpenPanel(initiallyOpen);
+	}
+
+	public void OpenPanel (Animator anim)
+	{
+		if (m_Open == anim)
+			return;
+
+		anim.gameObject.SetActive(true);
+		var newPreviouslySelected = EventSystem.current.currentSelectedGameObject;
+
+		anim.transform.SetAsLastSibling();
+
+		CloseCurrent();
+
+		m_PreviouslySelected = newPreviouslySelected;
+
+		m_Open = anim;
+		m_Open.SetBool(m_OpenParameterId, true);
+
+		GameObject go = FindFirstEnabledSelectable(anim.gameObject);
+
+		SetSelected(go);
+	}
+
+	static GameObject FindFirstEnabledSelectable (GameObject gameObject)
+	{
+		GameObject go = null;
+		var selectables = gameObject.GetComponentsInChildren<Selectable> (true);
+		foreach (var selectable in selectables) {
+			if (selectable.IsActive () && selectable.IsInteractable ()) {
+				go = selectable.gameObject;
+				break;
+			}
+		}
+		return go;
+	}
+
+	public void CloseCurrent()
+	{
+		if (m_Open == null)
+			return;
+
+		m_Open.SetBool(m_OpenParameterId, false);
+		SetSelected(m_PreviouslySelected);
+		StartCoroutine(DisablePanelDeleyed(m_Open));
+		m_Open = null;
+	}
+
+	IEnumerator DisablePanelDeleyed(Animator anim)
+	{
+		bool closedStateReached = false;
+		bool wantToClose = true;
+		while (!closedStateReached && wantToClose)
+		{
+			if (!anim.IsInTransition(0))
+				closedStateReached = anim.GetCurrentAnimatorStateInfo(0).IsName(k_ClosedStateName);
+
+			wantToClose = !anim.GetBool(m_OpenParameterId);
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		if (wantToClose)
+			anim.gameObject.SetActive(false);
+	}
+
+	private void SetSelected(GameObject go)
+	{
+		EventSystem.current.SetSelectedGameObject(go);
+	}
+}
